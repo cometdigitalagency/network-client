a1589979f6e41761bf0f08be22e07cf6a6787362
Arnaud Phommasone <arnaud.phommasone@gmail.com>
2022-12-02 16:01:44 +0700

    fix: add post type to BaseIntialWidget

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_generate_api_client/core/arguments/detail_arguments.dart';
 import 'package:flutter_generate_api_client/core/extensions/string_extension.dart';
+import 'package:flutter_generate_api_client/core/models/post.dart';
 import 'package:flutter_generate_api_client/core/models/post_params.dart';
 import 'package:flutter_generate_api_client/core/services/retrofit_client/retrofit_client.dart';
 import 'package:flutter_generate_api_client/widgets/base_initial_widget.dart';
@@ -17,7 +18,7 @@ class DetailPage extends StatelessWidget {
     final arg =
         ModalRoute.of(context)?.settings.arguments as DetailPageArguments;
     PostParams? postParams;
-    return BaseIntialWidget(
+    return BaseIntialWidget<Post>(
       future: restClient.getPost(arg.id),
       builder: (context, data) {
         postParams = data;
